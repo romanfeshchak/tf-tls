df1f6cd93cbe899befbba9f9b709d0dff7742b7e
romanfeshchak <147239057+romanfeshchak@users.noreply.github.com>
2024-05-11 09:58:09 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 output "private_key_pem" {
   sensitive = true
-  value = tls_private_key.terraincognitus.private_key_pem
+  value = tls_private_key.this.private_key_pem
 }
 
 output "public_key_openssh" {
   sensitive = true
-  value = tls_private_key.terraincognitus.public_key_openssh
+  value = tls_private_key.this.public_key_openssh
 }
 
